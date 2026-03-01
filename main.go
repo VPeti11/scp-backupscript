a1286448d77243565f4637eeb2e76a2b9d28bf9c
Mr. Bellringer <vpeti@vpeti.online>
2026-03-01 15:03:32 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,161 +1,166 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"io/fs"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
+    "context"
+    "fmt"
+    "io/fs"
+    "log"
+    "os"
+    "path/filepath"
+    "strings"
+    "time"
 
-	"github.com/bramvdbogaerde/go-scp"
-	"github.com/fsnotify/fsnotify"
-	"golang.org/x/crypto/ssh"
+    "github.com/bramvdbogaerde/go-scp"
+    "github.com/fsnotify/fsnotify"
+    "golang.org/x/crypto/ssh"
 )
 
 var (
-	remoteIP   = "serverip"
-	remotePort = "22"
-	sshUser    = "backupscp"
-	remoteRoot = "/opt/backup/"
-	localRoot = `F:\tobackup\`
-	privateKey = `-----BEGIN OPENSSH PRIVATE KEY-----
-	aaaaaaa
-	-----END OPENSSH PRIVATE KEY-----`
+    remoteIP    = "serverip"
+    remotePort  = "22"
+    sshUser     = "backupscp"
+    remoteRoot  = "/opt/backup/"
+    localRoot   = `F:\tobackup\`
+    privateKey  = `-----BEGIN OPENSSH PRIVATE KEY-----
+    aaaaaaa
+    -----END OPENSSH PRIVATE KEY-----`
+
+    SyncDeletions = true
+    DeletedFiles  = make(chan string, 100)
 )
 
-
 func main() {
-	watcher, err := fsnotify.NewWatcher()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer watcher.Close()
+    watcher, err := fsnotify.NewWatcher()
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer watcher.Close()
 
+    targetDir := filepath.FromSlash(localRoot)
+    if _, err := os.Stat(targetDir); os.IsNotExist(err) {
+        os.MkdirAll(targetDir, 0755)
+    }
 
-	targetDir := filepath.FromSlash(localRoot)
-	if _, err := os.Stat(targetDir); os.IsNotExist(err) {
-		os.MkdirAll(targetDir, 0755)
-	}
+    err = filepath.WalkDir(targetDir, func(path string, d fs.DirEntry, err error) error {
+        if err != nil {
+            return err
+        }
+        if d.IsDir() {
+            return watcher.Add(path)
+        }
+        return nil
+    })
+    if err != nil {
+        log.Fatal("Error walking path:", err)
+    }
 
+    fmt.Printf("Monitoring %s for changes...\n", targetDir)
 
-	err = filepath.WalkDir(targetDir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			return err
-		}
-		if d.IsDir() {
-			return watcher.Add(path)
-		}
-		return nil
-	})
-	if err != nil {
-		log.Fatal("Error walking path:", err)
-	}
+    for {
+        select {
+        case event, ok := <-watcher.Events:
+            if !ok {
+                return
+            }
 
-	fmt.Printf("Monitoring %s for changes...)\n", targetDir)
+            if event.Has(fsnotify.Remove) {
+                if SyncDeletions {
+                    DeletedFiles <- event.Name
+                }
+            }
 
-	for {
-		select {
-			case event, ok := <-watcher.Events:
-				if !ok {
-					return
-				}
+            if event.Has(fsnotify.Create) {
+                info, err := os.Stat(event.Name)
+                if err == nil && info.IsDir() {
+                    watcher.Add(event.Name)
+                    continue
+                }
+            }
 
+            if event.Has(fsnotify.Create) || event.Has(fsnotify.Write) {
+                info, err := os.Stat(event.Name)
+                if err == nil && !info.IsDir() {
+                    uploadFileWithRetry(event.Name, targetDir)
+                }
+            }
 
-				if event.Has(fsnotify.Create) {
-					info, err := os.Stat(event.Name)
-					if err == nil && info.IsDir() {
-						watcher.Add(event.Name)
-						log.Printf("[DIR] Now watching: %s", event.Name)
-						continue
-					}
-				}
-
-
-				if event.Has(fsnotify.Create) || event.Has(fsnotify.Write) {
-					info, err := os.Stat(event.Name)
-					if err == nil && !info.IsDir() {
-						log.Printf("[FILE] %v: %s", event.Op, event.Name)
-						uploadFile(event.Name, targetDir)
-					}
-				}
-
-			case err, ok := <-watcher.Errors:
-				if !ok {
-					return
-				}
-				log.Println("Watcher error:", err)
-		}
-	}
+        case err, ok := <-watcher.Errors:
+            if !ok {
+                return
+            }
+            log.Println("Watcher error:", err)
+        }
+    }
 }
 
-func uploadFile(localPath, baseDir string) {
+func uploadFileWithRetry(localPath, baseDir string) {
+    for attempt := 1; attempt <= 10; attempt++ {
+        err := uploadFile(localPath, baseDir)
+        if err == nil {
+            return
+        }
+        time.Sleep(time.Duration(attempt) * time.Second)
+    }
+}
 
-	cleanKey := strings.TrimSpace(privateKey)
-	lines := strings.Split(cleanKey, "\n")
-	for i, line := range lines {
-		lines[i] = strings.TrimSpace(line)
-	}
-	finalKey := strings.Join(lines, "\n")
+func uploadFile(localPath, baseDir string) error {
+    cleanKey := strings.TrimSpace(privateKey)
+    lines := strings.Split(cleanKey, "\n")
+    for i, line := range lines {
+        lines[i] = strings.TrimSpace(line)
+    }
+    finalKey := strings.Join(lines, "\n")
 
-	signer, err := ssh.ParsePrivateKey([]byte(finalKey))
-	if err != nil {
-		log.Printf("SSH Key Error: %v", err)
-		return
-	}
+    signer, err := ssh.ParsePrivateKey([]byte(finalKey))
+    if err != nil {
+        return err
+    }
 
-	sshConfig := &ssh.ClientConfig{
-		User:            sshUser,
-		Auth:            []ssh.AuthMethod{ssh.PublicKeys(signer)},
-		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-	}
+    sshConfig := &ssh.ClientConfig{
+        User:            sshUser,
+        Auth:            []ssh.AuthMethod{ssh.PublicKeys(signer)},
+        HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+    }
 
-	addr := fmt.Sprintf("%s:%s", remoteIP, remotePort)
-	client := scp.NewClient(addr, sshConfig)
+    addr := fmt.Sprintf("%s:%s", remoteIP, remotePort)
+    client := scp.NewClient(addr, sshConfig)
 
-	err = client.Connect()
-	if err != nil {
-		log.Printf("SSH Connection failed: %v", err)
-		return
-	}
-	defer client.Close()
+    err = client.Connect()
+    if err != nil {
+        return err
+    }
+    defer client.Close()
 
+    relPath, _ := filepath.Rel(baseDir, localPath)
+    remoteDest := filepath.ToSlash(filepath.Join(remoteRoot, relPath))
+    remoteDir := filepath.ToSlash(filepath.Dir(remoteDest))
 
-	relPath, _ := filepath.Rel(baseDir, localPath)
-	remoteDest := filepath.ToSlash(filepath.Join(remoteRoot, relPath))
-	remoteDir := filepath.ToSlash(filepath.Dir(remoteDest))
+    err = remoteMkdir(client.SSHClient(), remoteDir)
+    if err != nil {
+        return err
+    }
 
+    f, err := os.Open(localPath)
+    if err != nil {
+        return err
+    }
+    defer f.Close()
 
-	err = remoteMkdir(client.SSHClient(), remoteDir)
-	if err != nil {
-		log.Printf("Remote mkdir failed: %v", err)
-		return
-	}
+    err = client.CopyFromFile(context.Background(), *f, remoteDest, "0644")
+    if err != nil {
+        return err
+    }
 
-	f, err := os.Open(localPath)
-	if err != nil {
-		log.Printf("Local file error: %v", err)
-		return
-	}
-	defer f.Close()
-
-	err = client.CopyFromFile(context.Background(), *f, remoteDest, "0644")
-	if err != nil {
-		log.Printf("SCP Transfer error: %v", err)
-	} else {
-		log.Printf("Successfully uploaded -> %s", remoteDest)
-	}
+    return nil
 }
 
 func remoteMkdir(sshClient *ssh.Client, path string) error {
-	session, err := sshClient.NewSession()
-	if err != nil {
-		return err
-	}
-	defer session.Close()
+    session, err := sshClient.NewSession()
+    if err != nil {
+        return err
+    }
+    defer session.Close()
 
-
-	escapedPath := strings.ReplaceAll(path, "'", "\\'")
-	return session.Run(fmt.Sprintf("mkdir -p '%s'", escapedPath))
+    escapedPath := strings.ReplaceAll(path, "'", "\\'")
+    return session.Run(fmt.Sprintf("mkdir -p '%s'", escapedPath))
 }
